f3e67a79acd4125a8288de102ff55dd26ef961b5
Max Struever <madmaxlax@users.noreply.github.com>
2018-10-04 12:58:50 -0400

    Updates

---
+++ precache-manifest.887bec78a257618479aaee7d6c6b5975.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3226d04ec5295426596f",
+    "url": "/react-tictactoe-tutorial/static/js/runtime~main.3226d04e.js"
+  },
+  {
+    "revision": "698ce2f8493e321ca3bc",
+    "url": "/react-tictactoe-tutorial/static/js/main.698ce2f8.chunk.js"
+  },
+  {
+    "revision": "7e802ebce41dd84e0e5e",
+    "url": "/react-tictactoe-tutorial/static/js/1.7e802ebc.chunk.js"
+  },
+  {
+    "revision": "698ce2f8493e321ca3bc",
+    "url": "/react-tictactoe-tutorial/static/css/main.90c083ee.chunk.css"
+  },
+  {
+    "revision": "479fcecf54bb2adcfc74955fcc253f79",
+    "url": "/react-tictactoe-tutorial/index.html"
+  }
+];
